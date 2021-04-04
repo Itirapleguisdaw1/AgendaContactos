eb1ea4bb56b7416642cb6cfeecb169308da36fb7
elafrayagdaw <elafraygut1@educacion.navarra.es>
2021-04-04 21:41:39 +0200

    Clases enrique

--- src/Personal.java
+++ src/Personal.java
@@ -1,17 +1,85 @@
+import java.time.*;
+import java.util.*;
 
-public class Personal extends Contacto{
+/**
+ * 
+ * @author Unai Pérez, Iñaki Tiraplegui, Enrique Lafraya.
+ *
+ */
 
+public class Personal extends Contacto {
+LocalDate fecha;
+private static String firma;
+Relacion relacion;
+	public Personal(String nombre, String apellidos, String telefono,String email,LocalDate fecha,Relacion rel) {
+		super(nombre,apellidos,telefono,email);
+		this.fecha = fecha;
+		relacion = rel;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public Relacion getRelacion() {
+		return relacion;
+	}
+	public void setRelacion(Relacion relacion) {
+		this.relacion = relacion;
+	}
 	
-
-	public Personal(String nombre, String apellidos, String telefono, String email) {
-		super(nombre, apellidos, telefono, email);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
 	public String crearFirma() {
-		// TODO Auto-generated method stub
-		return null;
+		firma = "Un abrazo!";
+		return firma;
 	}
-
+	
+	public boolean esCumpleaños() {
+		LocalDate hoy = LocalDate.now();
+		boolean resultado = false;
+		if(hoy.equals(fecha)) {
+			resultado = true;
+		}
+		return resultado;
+	}
+	
+	private String formatearFecha() {
+		String formateada = "";
+		String mes = "";
+		switch(fecha.getMonthValue()) {
+		case 1: mes = "ener.";
+			break;
+		case 2: mes = "febr.";
+			break;
+		case 3: mes = "mar.";
+			break;
+		case 4: mes = "abr.";
+			break;
+		case 5: mes = "may.";
+			break;
+		case 6: mes = "jun";
+			break;
+		case 7: mes = "jul.";
+			break;
+		case 8: mes = "agost.";
+			break;
+		case 9: mes = "sept.";
+			break;
+		case 10: mes = "oct.";
+			break;
+		case 11: mes = "nov.";
+			break;
+		case 12: mes = "dic.";
+			break;
+		}
+		String dia = "" + fecha.getDayOfMonth();
+		String año = "" + fecha.getYear();
+		formateada = dia + " " + mes + " " + año;
+		return formateada;
+	}
+	public String toString() {
+		String resul = "";
+		resul = super.toString() + "\n" + "Fecha nacimiento: " +  formatearFecha() + "\n" +"Relacion: " + relacion + "\n\n" + crearFirma();
+		return resul;
+	}
 }
---
+++ src/Relacion.java
@@ -0,0 +1,4 @@
+
+public enum Relacion {
+PADRE,MADRE,AMIGOS,HIJO,HIJA,PAREJA
+}
