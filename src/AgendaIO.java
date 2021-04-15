16d8d07168b4f62a5d976be1c023bfb5617ff0c3
Itirapleguisdaw1 <itirapleguisdaw1@gmail.com>
2021-04-15 02:08:21 +0200

    Clase AgendaIO sin completar del todo

--- src/AgendaIO.java
+++ src/AgendaIO.java
@@ -5,12 +5,49 @@
 public class AgendaIO {
 
 	public static void importar(AgendaContactos agenda) {
-
+		obtenerLineasDatos();
 	}
 
 	private static Contacto parsearLinea(String linea) {
-		return null;
-
+		
+		String[] separacion = linea.split(",");
+		for(int i = 0; i < separacion.length -1 ; i++) {
+			separacion[i].trim();
+		}
+		String nombre = separacion[1];
+		String apellidos = separacion[2];
+		String telefono = separacion[3];
+		String email = separacion[4];
+		String tempo1 = separacion[5];
+		Relacion relac = Relacion.HIJO;
+		if(separacion[0] == "1"){
+			Profesional tempo = new Profesional(nombre, apellidos, telefono, email, tempo1); 
+			return tempo;
+		}
+		if(separacion[0] == "2") {
+			switch(separacion[6]) {
+			case "padre":
+				relac = relac.PADRE;
+				break;
+			case "madre":
+				relac = relac.MADRE;
+				break;
+			case "amigos":
+				relac = relac.AMIGOS;
+				break;
+			case "hijo":
+				relac = relac.HIJO;
+				break;
+			case "hija":
+				relac = relac.HIJA;
+				break;
+			case "":
+				relac = relac.PADRE;
+				break;
+			}
+			Personal tempo = new Personal(nombre, apellidos, telefono, email, tempo1, relac);
+			return tempo;
+		}
 	}
 
 	/**
@@ -21,28 +58,28 @@ public class AgendaIO {
 	 */
 	private static String[] obtenerLineasDatos() {
 		return new String[] {
-				"1, Isabel, Acosta Mendioroz,  678895433 ,  iacostamen@gmail.com ,  walden estrella ",
-				"2,  pedro , urruti tello , 616789654 ,  urrutitello@gmail.com , 09/03/2007, amigos",
-				"1, Angel , Esteban Grande , 674544123 ,  aestebang@gmail.com ,  magma publicidad ",
-				"2, elena , bueno ganuza , 6786547699 ,  ebuenogan@gmail.com , 17/03/2000, amigos",
-				"2, amaia , romero sein , 642222343 ,  aromerosein@gmail.com , 09/03/2012, pareja",
-				"2, Ignacio ,  Anto roth ,  688912799 , iantoroth@gmail.com ,  11/11/1969 , padre",
-				"1,  Isabel ,  Acosta Marin , 678895433 ,  iacostamar@gmail.com ,  publicidad holdings ",
-				"1 ,    roberto , casas maura , 666777888 ,  rocasasma@gmail.com ,  strato banca ",
+				"1, Isabel, Acosta Mendioroz, 678895433 , iacostamen@gmail.com , walden estrella ",
+				"2, pedro , urruti tello , 616789654 , urrutitello@gmail.com , 09/03/2007, amigos",
+				"1, Angel , Esteban Grande , 674544123 , aestebang@gmail.com , magma publicidad ",
+				"2, elena , bueno ganuza , 6786547699 , ebuenogan@gmail.com , 17/03/2000, amigos",
+				"2, amaia , romero sein , 642222343 , aromerosein@gmail.com , 09/03/2012, pareja",
+				"2, Ignacio , Anto roth ,  688912799 , iantoroth@gmail.com , 11/11/1969 , padre",
+				"1, Isabel , Acosta Marin , 678895433 , iacostamar@gmail.com , publicidad holdings ",
+				"1 , roberto , casas maura , 666777888 , rocasasma@gmail.com , strato banca ",
 				"1,juan maria, garcía oliva, 699898111, jmgarcioliva@gmail.com, conway motor ",
-				"2, pedro , urruti tello , 616789654 ,  urrutitello@gmail.com , 17/03/2000, amigos",
+				"2, pedro , urruti tello , 616789654 , urrutitello@gmail.com , 17/03/2000, amigos",
 				"1,marta, sanz iris, 622999876, msanzi@gmail.com, jump literatura ",
 				"1,javier, porto luque, 691256777 , japorlu@gmail.com, gas natural ",
 				"1,pablo, ponce larraoz, 689123456, pabloponce@gmail.com, la caixa",
 				"1, javier, marin lancho, 666666666, jruizlanchoe@gmail.com, bbva",
 				"1,juan maria, garcía oliva, 699898111, jmgarcioliva@gmail.com, conway motor ",
-				"2, Berta ,  andia solano ,  621123345 , bandiasol@gmail.com ,  12/12/1999 ,  HIJA",
-				"2, Ignacio ,  Anto roth ,  688912799 , iantoroth@gmail.com ,  11/11/1969 , padre",
-				"  1,  roberto , casas maura , 666777888 ,  rocasasma@gmail.com ,  strato banca ",
-				" 2, daniel , martin martin , 678901234 ,  damrtinmartin@gmail.com , 15/07/1980, amigos",
-				"  2, pablo , martin abradelo , 667788899 ,  martinabra@gmail.com , 31/01/2010, amigos",
-				"  2, susana , santaolalla bilbao , 676767676 ,  ssantaolalla@gmail.com , 17/03/1998, amigos",
-				"  2, adur ,  martin merino ,  611112113 , adurmartinme@gmail.com ,  14/02/2003 , amigos" };
+				"2, Berta , andia solano , 621123345 , bandiasol@gmail.com , 12/12/1999 , HIJA",
+				"2, Ignacio , Anto roth , 688912799 , iantoroth@gmail.com , 11/11/1969 , padre",
+				" 1, roberto , casas maura , 666777888 , rocasasma@gmail.com , strato banca ",
+				" 2, daniel , martin martin , 678901234 , damrtinmartin@gmail.com , 15/07/1980, amigos",
+				" 2, pablo , martin abradelo , 667788899 , martinabra@gmail.com , 31/01/2010, amigos",
+				" 2, susana , santaolalla bilbao , 676767676 , ssantaolalla@gmail.com , 17/03/1998, amigos",
+				" 2, adur , martin merino , 611112113 , adurmartinme@gmail.com , 14/02/2003 , amigos" };
 
 	}
 
